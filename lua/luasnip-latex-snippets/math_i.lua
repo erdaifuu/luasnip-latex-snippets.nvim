b452ca53c08eafec3d8b276ab35c7438cf56fdb6
Carl Luo <91815967+erdaifuu@users.noreply.github.com>
2024-09-09 14:24:29 -0700

    Update math_i.lua

--- lua/luasnip-latex-snippets/math_i.lua
+++ lua/luasnip-latex-snippets/math_i.lua
@@ -40,11 +40,11 @@ function M.retrieve(is_math)
 
     -- parse_snippet(
     --   { trig = "lr", name = "left( right)" },
-    --   "\\left( ${1:${TM_SELECTED_TEXT}} \\right) $0"
+    --   "\\left( ${1:${TM_SELECTED_TEXT}} \\right $0"
     -- ),
     parse_snippet(
     { trig = "lr(", name = "left( right)" },
-    "\\left( ${1:${TM_SELECTED_TEXT}} \\right)$0"
+    "\\left( ${1:${TM_SELECTED_TEXT}} \\right$0"
     ),
     parse_snippet(
     { trig = "lr|", name = "left| right|" },
@@ -52,11 +52,11 @@ function M.retrieve(is_math)
     ),
     parse_snippet(
     { trig = "lr{", name = "left{ right}" },
-    "\\left\\{ ${1:${TM_SELECTED_TEXT}} \\right\\\\}$0"
+    "\\left\\{ ${1:${TM_SELECTED_TEXT}} \\right\\$0"
     ),
     parse_snippet(
     { trig = "lr[", name = "left[ right]" },
-    "\\left[ ${1:${TM_SELECTED_TEXT}} \\right]$0"
+    "\\left[ ${1:${TM_SELECTED_TEXT}} \\right$0"
     ),
     parse_snippet(
     { trig = "lra", name = "leftangle rightangle" },
