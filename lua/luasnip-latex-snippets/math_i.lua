0523cd2e58d8dbed04352ae2388e4364bf8b7cfd
Carl Luo <91815967+erdaifuu@users.noreply.github.com>
2024-09-13 14:59:21 -0700

    Update math_i.lua

--- lua/luasnip-latex-snippets/math_i.lua
+++ lua/luasnip-latex-snippets/math_i.lua
@@ -65,7 +65,7 @@ function M.retrieve(is_math)
 
     parse_snippet(
     { trig = "lrb", name = "left\\{ right\\}" },
-    "\\left\\{ ${1:${TM_SELECTED_TEXT}} \\right\\\\}$0"
+    "\\left\\{ ${1:${TM_SELECTED_TEXT}} \\right\\}$0"
     ),
 
     parse_snippet(
